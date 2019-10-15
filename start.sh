94bcbd4c9c4f401b9a98347a0ec6ba2a5316698a
esrrhs <esrrhs@163.com>
2019-10-15 09:49:16 +0800

    add

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull
 docker stop my-nginx
 docker rm my-nginx
-docker run --name my-nginx --restart=always -p 80:80 -v /home/project/esrrhs.xyz/conf/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/project/esrrhs.xyz/html:/usr/share/nginx/html:ro -d nginx
+docker run --name my-nginx --restart=always -p 80:80 -v /home/project/esrrhs.xyz/conf/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/project/esrrhs.xyz/html:/usr/share/nginx/html:ro -d nginx:latest
