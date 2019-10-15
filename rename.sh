bd2a23a4e124e4e2ec4bae4704e600f5e731c94a
esrrhs <esrrhs@163.com>
2019-10-15 20:42:39 +0800

    add

--- rename.sh
+++ rename.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
-
 if [ $# != 1 ];then
     echo "need new name, eg. .com .ml .cf"
     exit
 fi
-
+set -x
 newname=$1
 echo $newname
 grep esrrhs.xyz * -rl | grep -v .sh | xargs sed 's#.xyz#'$newname'#g' -i
